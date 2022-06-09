2c2fc400a6cf09640bf91603901e16a239034847
TrungHauDinh <trunghaudinh96@gmail.com>
2022-06-09 07:46:10 +0700

    update 8-6

--- app/src/main/java/com/example/sticker2d/MainActivity.kt
+++ app/src/main/java/com/example/sticker2d/MainActivity.kt
@@ -1,63 +1,46 @@
 package com.example.sticker2d
 
 import android.Manifest
-import android.content.ContentValues
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleOwner
+import com.bazooka.libcamerax.CameraManager
 import com.example.sticker2d.databinding.ActivityMainBinding
-import com.google.mlkit.vision.face.FaceDetectorOptions
-import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.Executor
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LifecycleOwner {
     private lateinit var binding: ActivityMainBinding
-    private var imageCapture: ImageCapture? = null
-    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var cameraXManager: CameraManager
 
-    private lateinit var cameraManager : CameraManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        cameraManager = CameraManager(this,binding.viewFinder,this)
-
+        cameraXManager = CameraManager(this, binding.viewFinder, this)
         if (allPermissionsGranted()) {
-            cameraManager.startCamera()
+            cameraXManager.startCamera()
         } else {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
 
         binding.btnTake.setOnClickListener {
             Log.i("CameraX_TAG", "onCreate:2222 ")
-            cameraManager.takePhoto()
         }
 
         binding.btnSwitch.setOnClickListener {
             Log.i("CameraX_TAG", "onCreate:2222 ")
-            cameraManager.changeCameraSelector()
+            cameraXManager.changeCameraSelector()
         }
 
 
-
     }
-
 
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -72,7 +55,7 @@ class MainActivity : AppCompatActivity() {
     ) {
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
-                cameraManager.startCamera()
+                cameraXManager.startCamera()
             } else {
                 Toast.makeText(
                     this,
@@ -89,7 +72,7 @@ class MainActivity : AppCompatActivity() {
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS =
-            mutableListOf (
+            mutableListOf(
                 Manifest.permission.CAMERA,
                 Manifest.permission.RECORD_AUDIO
             ).apply {
